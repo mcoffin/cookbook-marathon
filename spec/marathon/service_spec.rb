d4ae7927eacf0c8c1a8b3a5b5ab92e4f573ff29b
Matt Coffin <mcoffin13@gmail.com>
2015-09-08 10:33:16 -0600

    Add rubocop checks

--- spec/marathon/service_spec.rb
+++ spec/marathon/service_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe 'marathon::service on ubuntu-14.04' do
-  let(:chef_run) { ChefSpec::SoloRunner.new(
-    platform: 'ubuntu',
-    version: '14.04'
-  ).converge('marathon::service') }
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new(
+      platform: 'ubuntu',
+      version: '14.04'
+    ).converge('marathon::service')
+  end
 
   it 'creates service[marathon]' do
     expect(chef_run).to start_service('marathon')
