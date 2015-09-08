c723c8c4a5f38997bf942c7ec8569b3493e87eeb
Matt Coffin <mcoffin13@gmail.com>
2015-09-08 12:12:46 -0600

    Add defaults for work_dir and log_dir

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,10 @@ default['marathon']['version'] = '0.10.1'
 default['marathon']['checksum'] = '0c4c78058a26194cf039614e3f88ab4746a534a372cc547e511972909397bf5b'
 default['marathon']['path'] = '/opt/marathon'
 default['marathon']['restart_sec'] = '20'
-default['marathon']['options'] = []
+default['marathon']['options'] = {
+  work_dir: '/var/lib/mesos/slave',
+  log_dir: '/var/log/mesos/slave'
+}
 
 default['marathon']['init'] = case node['platform']
                               when 'ubuntu' then
