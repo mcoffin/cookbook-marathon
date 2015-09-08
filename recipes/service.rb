6fddef320cad38ce5b5e282738c520eb77d56a2a
Matt Coffin <mcoffin13@gmail.com>
2015-09-08 15:43:16 -0600

    Use chefstart file for systemd as well

--- recipes/service.rb
+++ recipes/service.rb
@@ -5,25 +5,30 @@ node['marathon']['options'].each do |opt, value|
   start_command << " --#{opt} #{value}"
 end
 
+chefstart = "#{node['marathon']['path']}/bin/chef-start"
+file chefstart do
+  content start_command
+  mode '0755'
+  owner 'root'
+  group 'root'
+  notifies :restart, 'service[marathon]', :delayed
+end
+
 case node['marathon']['init']
 when 'systemd'
   systemd_service 'marathon' do
     after 'network.target'
     wants 'network.target'
     description 'Marathon framework'
-    exec_start start_command
+    exec_start chefstart
     restart 'always'
     restart_sec node['marathon']['restart_sec']
     action [:create, :enable, :start]
   end
-when 'upstart'
-  chefstart = "#{node['marathon']['path']}/bin/chef-start"
-  file chefstart do
-    content start_command
-    mode '0755'
-    owner 'root'
-    group 'root'
+  service 'marathon' do
+    action [:nothing]
   end
+when 'upstart'
   template '/etc/init/marathon.conf' do
     source 'upstart.erb'
     variables(command: chefstart)
