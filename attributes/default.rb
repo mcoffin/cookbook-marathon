eb9533f8b62db2bb3d08edfa04336cb48c9c5ad6
Matt Coffin <mcoffin13@gmail.com>
2015-09-08 13:59:59 -0600

    Remove incorrect usage of mesos options

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,7 @@ default['marathon']['version'] = '0.10.1'
 default['marathon']['checksum'] = '0c4c78058a26194cf039614e3f88ab4746a534a372cc547e511972909397bf5b'
 default['marathon']['path'] = '/opt/marathon'
 default['marathon']['restart_sec'] = '20'
-default['marathon']['options'] = {
-  work_dir: '/var/lib/mesos/slave',
-  log_dir: '/var/log/mesos/slave'
-}
+default['marathon']['options'] = {}
 
 default['marathon']['init'] = case node['platform']
                               when 'ubuntu' then
