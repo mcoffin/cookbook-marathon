f2ceca9985132d7c1b0fccdd3147bf4c8865274c
Matt Coffin <matt.coffin@charter.com>
2017-02-03 15:17:47 -0700

    upgrade systemd
    0.2.0 -> 2.1.3

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures marathon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'systemd', '~> 0.2.0'
+depends 'systemd', '~> 2.1.3'
